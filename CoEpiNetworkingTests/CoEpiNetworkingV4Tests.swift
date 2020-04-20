da51aea371b3098cfe00568b7c94557d926ec99f
Dusko Ojdanic <dusko.ojdanic@gmail.com>
2020-04-20 12:32:46 +0200

    CoEpiNetworkingV4Tests - testV4GetTcnReportWithIntervalNumber

--- CoEpiNetworkingTests/CoEpiNetworkingV4Tests.swift
+++ CoEpiNetworkingTests/CoEpiNetworkingV4Tests.swift
@@ -44,9 +44,19 @@ class CoEpiNetworkingV4Tests: XCTestCase {
     
     private func executeGet(url: String){
         let expect = expectation(description: "request complete")
-        let session = Session(eventMonitors: [ AlamofireLogger() ])
         
-        let _ = session.request(url).responseJSON { response in
+        //Fix for error message: "CredStore - performQuery - Error copying matching creds.  Error=-25300" See: https://stackoverflow.com/a/54100650
+        let configuration = URLSessionConfiguration.default
+        configuration.urlCredentialStorage = nil
+
+        let session = Session(configuration: configuration,  eventMonitors: [ AlamofireLogger()])
+        let _ = session.request(url)
+            .cURLDescription { description in
+                print(description)
+            }
+            .responseJSON { response in
+             let statusCode = response.response?.statusCode
+            print("statusCode : [\(statusCode!)]")
             expect.fulfill()
             switch response.result {
             case .success(let JSON):
@@ -60,7 +70,7 @@ class CoEpiNetworkingV4Tests: XCTestCase {
             
         }
         
-        waitForExpectations(timeout: 5)
+        waitForExpectations(timeout: 10)
 
     }
     
@@ -68,9 +78,38 @@ class CoEpiNetworkingV4Tests: XCTestCase {
      curl -X GET https://18ye1iivg6.execute-api.us-west-1.amazonaws.com/v4/tcnreport?date=2020-04-19
      */
     func testV4GetTcnReportWithDate() {
-        let url: String = apiV4 + "/tcnreport?date=2020-04-19"
+        let url: String = apiV4 + "/tcnreport?date=2020-04-18"
         executeGet(url: url)
     }
+    
+    /**
+     GET /tcnreport?date={report_date}?intervalNumber={interval}?intervalLengthMs={interval_length_ms}
+     */
+    
+    /**
+     formatedDate : [2020-04-20 01:28:04 +0200]
+     formatedUtcDate : [2020-04-19 23:28:04 +0000]
+     */
+    func testV4GetTcnReportWithIntervalNumber() {
+        let currentDate = Date()
+        let intervalLengthMillis : Int64 = 6 * 3600 * 1000
+        let currentMillis = Int64(currentDate.timeIntervalSince1970 * 1000)
+        let intervalNumber = currentMillis / intervalLengthMillis
+        let dateformater = DateFormatter()
+        dateformater.dateFormat = "yyyy-MM-dd"//"yyyy-MM-dd HH:mm:ss ZZZ"
+        dateformater.timeZone = TimeZone(abbreviation: "UTC")
+        let formatedDate = dateformater.string(from: currentDate)
+        let url: String = apiV4 + "/tcnreport?date=\(formatedDate)&intervalNumber=\(intervalNumber)&intervalLengthMs=\(intervalLengthMillis)"
+        
+        print("intervalLengthMillis : [\(intervalLengthMillis)]")
+        print("currentMillis : [\(currentMillis)]")
+        print("intervalNumber : [\(intervalNumber)]")
+        print("formatedDate : [\(formatedDate)]")
+        print("url : [\(url)]")
+
+        executeGet(url: url)
+    }
+    
     
     /**
      curl -X POST https://18ye1iivg6.execute-api.us-west-1.amazonaws.com/v4/tcnreport -d "ZXlKMFpYTjBJam9pWW05a2VTSjk="
@@ -82,7 +121,7 @@ class CoEpiNetworkingV4Tests: XCTestCase {
         let session = Session(eventMonitors: [ AlamofireLogger() ])
         let paramsString = "Test payload \(Date().timeIntervalSince1970)"
         let paramsStringEncoded = Data(paramsString.utf8).base64EncodedString()
-        
+        //https://github.com/Alamofire/Alamofire/blob/master/Documentation/AdvancedUsage.md#urlrequestconvertible
         let url = URL(string: urlString)!
         var urlRequest = URLRequest(url: url)
         urlRequest.method = .post
